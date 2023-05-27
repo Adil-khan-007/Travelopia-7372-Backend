56cc2c47b1da511e57350a1ffafd58e3f144c8bf
Adil-khan-007 <adilk9030@gmail.com>
2023-05-27 14:45:05 +0530

    backend completed

--- configs/connectDatabase.js
+++ configs/connectDatabase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDatabase =  async (req,res)=>{
-    return mongoose.connect();
+    return mongoose.connect(process.env.MONGODB);
 }
 
 module.exports = connectDatabase;
